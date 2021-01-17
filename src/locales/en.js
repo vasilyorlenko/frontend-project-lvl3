f3a4d8b50944937bffbf9a07f38116ebd955fb61
Vasily Orlenko <orlenko.vasily.p@gmail.com>
2021-01-17 21:55:08 +0300

    change warning message for invalid url

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,7 +1,7 @@
 export default {
   translation: {
     errorMessages: {
-      invalidUrl: 'URL is not valid',
+      invalidUrl: 'Must be valid url',
       urlAlreadyAdded: 'Source has already been added',
       networkError: 'Network request failed',
       parsingError: 'Source does not contain valid RSS',
