949a1fa227b04a1bb93c993e0a4bdbd0779e2992
Vasily Orlenko <orlenko.vasily.p@gmail.com>
2021-01-17 19:39:59 +0300

    add resources for i18next

---
+++ src/locales/en.js
@@ -0,0 +1,11 @@
+export default {
+  translation: {
+    errorMessages: {
+      invalidUrl: 'URL is not valid',
+      urlAlreadyAdded: 'Source has already been added',
+      networkError: 'Network request failed',
+      parsingError: 'Source does not contain valid RSS',
+    },
+    successMessage: 'New RSS feed has been loaded',
+  },
+};
